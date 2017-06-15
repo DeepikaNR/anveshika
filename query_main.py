b496f7f707ac6d9c48c1a472ac3a830cd8eebb4c
DeepikaNR <deepikanr4@gmail.com>
2017-06-15 22:57:02 +0530

    Update query_main.py

--- query_main.py
+++ query_main.py
@@ -30,44 +30,4 @@ def start_query(query_text):
 
     return result_list
 
-    """
-    q = Query(["/Users/deepikaravi/yojana/anveshika/harvest/54.txt", "/Users/deepikaravi/yojana/anveshika/harvest/56.txt", "/Users/deepikaravi/yojana/anveshika/harvest/60.txt", "/Users/deepikaravi/yojana/anveshika/harvest/102.txt","/Users/deepikaravi/yojana/anveshika/harvest/100.txt","/Users/deepikaravi/yojana/anveshika/harvest/101.txt",
-               "/Users/deepikaravi/yojana/anveshika/harvest/120.txt","/Users/deepikaravi/yojana/anveshika/harvest/121.txt","/Users/deepikaravi/yojana/anveshika/harvest/122.txt"])
-
-    print "*" * 100
-    print "One word query : Positive test case: Existing word: |Sringeri| "
-    print "*" * 100
-    print q.print_url(q.one_word_query("sringeri"))
-    print "\n\n\n"
-
-    print "*" * 100
-    print "One word query : Negative test case: Non-Existing word: |SriLanka| "
-    print "*" * 100
-    print q.print_url(q.one_word_query("SriLanka"))
-    print "\n\n\n"
-
-    print "*" * 100
-    print "Phrase query : Positive test case: Existing phrase: |Coastal Karnataka| "
-    print "*" * 100
-    print q.print_url(q.phrase_query("Coastal Karnataka"))
-    print "\n\n\n"
-
-    print "*" * 100
-    print "Phrase query : Negative test case: Non-Existing phrase: |Himalayan ranges| "
-    print "*" * 100
-    print q.print_url(q.phrase_query("Himalayan ranges"))
-    print "\n\n\n"
-
-    print "*" * 100
-    print "Free text query : Positive test case: Existing text: |national language india| "
-    print "*" * 100
-    print q.print_url(q.free_text_query("national language india"))
-    print "\n\n\n"
-
-    print "*" * 100
-    print "Free text query : Negative test case: Non-Existing text: |banjar| "
-    print "*" * 100
-    print q.print_url(q.free_text_query("address"))
-    print "\n\n\n"
-
-    """
+    
